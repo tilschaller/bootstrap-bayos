012d064606b517e1ebdb3b7339ba2a834cc7ddc5
Til Schaller <schaller.til@gmail.com>
2026-09-11 21:41:17 +0200

    Modified the init process slightly

--- bayos-init/main.c
+++ bayos-init/main.c
@@ -2,6 +2,13 @@
 
 int main(void) {
 	printf("Hello world, from userspace!\n");
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	for (int i = 0; i < 10; i++) {
+		int c = getchar();
+		putchar(c);
+	}
 	
 	return 0;
 }
