266d1fbeae12a7031283ad7ec673bf92431ff632
Til Schaller <schaller.til@gmail.com>
2026-09-13 20:21:13 +0200

    added fork syscall

--- bayos-init/main.c
+++ bayos-init/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-	printf("Hello world, from userspace!\n");
-
 	setvbuf(stdout, NULL, _IONBF, 0);
+	printf("Hello world, from userspace!\n");
 
 	for (int i = 0; i < 10; i++) {
 		int c = getchar();
 		putchar(c);
 	}
-	
+	putchar('\n');
+
+	pid_t p = fork();
+	if (p == 0) {
+		printf("Hello from child process!\n");
+	} else {
+		printf("Hello from parent process!\n");
+	}
+
 	return 0;
 }
